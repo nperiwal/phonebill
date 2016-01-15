fa4ef6d8eba0644c06c4bcc91233bb8b01a76cb3
nperiwal <narayan.periwal@inmobi.com>
2016-01-15 17:36:28 +0530

    Removed Demo Class

--- app/src/main/java/com/example/oozie/phonebill3/DemoActivity.java
+++ app/src/main/java/com/example/oozie/phonebill3/DemoActivity.java
@@ -1,7 +1,5 @@
 package com.example.oozie.phonebill3;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -12,33 +10,23 @@ import android.view.ViewGroup;
 
 public class DemoActivity extends AppCompatActivity {
 
-    private static final String KEY_DEMO = "demo";
-
-    public static void startActivity(Context context, Demo demo) {
-        Intent intent = new Intent(context, DemoActivity.class);
-        intent.putExtra(KEY_DEMO, demo.name());
-        context.startActivity(intent);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
 
-        Demo demo = getDemo();
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle(demo.titleResId);
+        toolbar.setTitle(R.string.demo_title_basic);
 
         ViewGroup tab = (ViewGroup) findViewById(R.id.tab);
-        tab.addView(LayoutInflater.from(this).inflate(demo.layoutResId, tab, false));
+        tab.addView(LayoutInflater.from(this).inflate(R.layout.demo_basic, tab, false));
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
-        demo.setup(viewPagerTab);
+        setup(viewPagerTab);
 
         FragmentPagerItems pages = new FragmentPagerItems(this);
-        for (int titleResId : demo.tabs()) {
+        for (int titleResId : tabs()) {
             pages.add(FragmentPagerItem.of(getString(titleResId), DemoFragment.class));
         }
 
@@ -50,7 +38,19 @@ public class DemoActivity extends AppCompatActivity {
 
     }
 
-    private Demo getDemo() {
-        return Demo.valueOf("BASIC");
+    private void setup(final SmartTabLayout layout) {
+        //Do nothing.
+    }
+
+    private static int[] tab3() {
+        return new int[] {
+                R.string.tab_1,
+                R.string.tab_2,
+                R.string.tab_3
+        };
+    }
+
+    private int[] tabs() {
+        return tab3();
     }
 }
