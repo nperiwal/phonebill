3dbd66191565829311b3200aed088894d65b6939
nperiwal <narayan.periwal@inmobi.com>
2016-01-15 19:47:34 +0530

    Fetching Call Details

--- app/src/main/java/com/example/oozie/phonebill3/DemoActivity.java
+++ app/src/main/java/com/example/oozie/phonebill3/DemoActivity.java
@@ -1,14 +1,27 @@
 package com.example.oozie.phonebill3;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.CallLog;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import java.util.Date;
+
 
 public class DemoActivity extends AppCompatActivity {
+
+    private String callText;
+
+    public String getCallText() {
+        return callText;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +30,8 @@ public class DemoActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.demo_title_basic);
+
+        callText = getCallDetails();
 
         ViewGroup tab = (ViewGroup) findViewById(R.id.tab);
         tab.addView(LayoutInflater.from(this).inflate(R.layout.demo_basic, tab, false));
@@ -43,8 +58,8 @@ public class DemoActivity extends AppCompatActivity {
     }
 
     private static int[] tab3() {
-        return new int[] {
-                R.string.tab_1,
+        return new int[]{
+                //R.string.tab_1,
                 R.string.tab_2,
                 R.string.tab_3
         };
@@ -52,5 +67,58 @@ public class DemoActivity extends AppCompatActivity {
 
     private int[] tabs() {
         return tab3();
+    }
+
+    private String getCallDetails() {
+        StringBuffer sb = new StringBuffer();
+        String strOrder = android.provider.CallLog.Calls.DATE + " DESC";
+
+        /* Query the CallLog Content Provider */
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) !=
+                PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return "";
+        }
+        Cursor managedCursor = getContentResolver().query(CallLog.Calls.CONTENT_URI, null, null,
+                null, strOrder);
+        int number = managedCursor.getColumnIndex(CallLog.Calls.NUMBER);
+        int type = managedCursor.getColumnIndex(CallLog.Calls.TYPE);
+        int date = managedCursor.getColumnIndex(CallLog.Calls.DATE);
+        int duration = managedCursor.getColumnIndex(CallLog.Calls.DURATION);
+        //int account = managedCursor.getColumnIndex(CallLog.Calls.PHONE_ACCOUNT_COMPONENT_NAME);
+        while (managedCursor.moveToNext()) {
+            String phNum = managedCursor.getString(number);
+            String callTypeCode = managedCursor.getString(type);
+            String strcallDate = managedCursor.getString(date);
+            Date callDate = new Date(Long.valueOf(strcallDate));
+            String callDuration = managedCursor.getString(duration);
+            String callType = null;
+            int callcode = Integer.parseInt(callTypeCode);
+            switch (callcode) {
+                case CallLog.Calls.OUTGOING_TYPE:
+                    callType = "Outgoing";
+                    break;
+                case CallLog.Calls.INCOMING_TYPE:
+                    callType = "Incoming";
+                    break;
+                case CallLog.Calls.MISSED_TYPE:
+                    callType = "Missed";
+                    break;
+            }
+            //String accountName = managedCursor.getString(account);
+            sb.append("\nPhone Number:--- " + phNum + " \nCall Type:--- "
+                    + callType + " \nCall Date:--- " + callDate
+                    + " \nCall duration in sec :--- " + callDuration);
+                    //+ " \nOwner :--- " + accountName);
+            sb.append("\n----------------------------------");
+        }
+        return sb.toString();
     }
 }
--- app/src/main/java/com/example/oozie/phonebill3/DemoFragment.java
+++ app/src/main/java/com/example/oozie/phonebill3/DemoFragment.java
@@ -3,6 +3,7 @@ package com.example.oozie.phonebill3;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,18 +11,51 @@ import android.widget.TextView;
 
 public class DemoFragment extends Fragment {
 
+    private String TAG = "DemoFragment";
+    private String CALL_TEXT = "callText";
+
+    private TextView textView;
+    private String callText;
+
+    boolean visitFlag3 = false;
+
+    public TextView getTextView() {
+        return textView;
+    }
+
+    public void setCallText(String callText) {
+        this.callText = callText;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.v(TAG, "In frag's on save instance state ");
+        outState.putSerializable(CALL_TEXT, callText);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        Log.v(TAG, "In frag's on create view");
         return inflater.inflate(R.layout.fragment_demo, container, false);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        Log.v(TAG, "In frag's on view created");
         super.onViewCreated(view, savedInstanceState);
         int position = FragmentPagerItem.getPosition(getArguments());
-        TextView title = (TextView) view.findViewById(R.id.item_title);
-        title.setText(String.valueOf(position));
+        System.out.println("Tab Position: " + String.valueOf(position));
+        textView = (TextView) view.findViewById(R.id.call_log_text);
+        if (position == 1) {
+            DemoActivity activity = (DemoActivity) getActivity();
+            callText = activity.getCallText();
+            textView = (TextView) view.findViewById(R.id.call_log_text);
+            textView.setText(callText);
+        } else {
+            textView.setText(String.valueOf(position));
+        }
     }
 
 }
