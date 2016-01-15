cbe4953e07b91b34bc10d21329ad88365cc38d8a
nperiwal <narayan.periwal@inmobi.com>
2016-01-15 16:39:37 +0530

    Phone Bill Optimizer

---
+++ app/src/main/java/com/example/oozie/phonebill3/Demo.java
@@ -0,0 +1,48 @@
+package com.example.oozie.phonebill3;
+
+import android.content.Context;
+
+public enum Demo {
+
+    BASIC(R.string.demo_title_basic, R.layout.demo_basic);
+
+    public final int titleResId;
+    public final int layoutResId;
+
+    Demo(int titleResId, int layoutResId) {
+        this.titleResId = titleResId;
+        this.layoutResId = layoutResId;
+    }
+
+    public static int[] tab10() {
+        return new int[] {
+                R.string.demo_tab_5,
+                R.string.demo_tab_6,
+                R.string.demo_tab_7,
+                R.string.demo_tab_8,
+                R.string.demo_tab_9,
+                R.string.demo_tab_10
+        };
+    }
+
+    public static int[] tab3() {
+        return new int[] {
+                R.string.demo_tab_8,
+                R.string.demo_tab_9,
+                R.string.demo_tab_10
+        };
+    }
+
+    public void startActivity(Context context) {
+        DemoActivity.startActivity(context, this);
+    }
+
+    public void setup(final SmartTabLayout layout) {
+        //Do nothing.
+    }
+
+    public int[] tabs() {
+        return tab10();
+    }
+
+}
---
+++ app/src/main/java/com/example/oozie/phonebill3/DemoActivity.java
@@ -0,0 +1,59 @@
+package com.example.oozie.phonebill3;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+
+public class DemoActivity extends AppCompatActivity {
+
+    private static final String KEY_DEMO = "demo";
+
+    public static void startActivity(Context context, Demo demo) {
+        Intent intent = new Intent(context, DemoActivity.class);
+        intent.putExtra(KEY_DEMO, demo.name());
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+
+        Demo demo = getDemo();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(demo.titleResId);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ViewGroup tab = (ViewGroup) findViewById(R.id.tab);
+        tab.addView(LayoutInflater.from(this).inflate(demo.layoutResId, tab, false));
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
+        demo.setup(viewPagerTab);
+
+        FragmentPagerItems pages = new FragmentPagerItems(this);
+        for (int titleResId : demo.tabs()) {
+            pages.add(FragmentPagerItem.of(getString(titleResId), DemoFragment.class));
+        }
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), pages);
+
+        viewPager.setAdapter(adapter);
+        viewPagerTab.setViewPager(viewPager);
+
+    }
+
+    private Demo getDemo() {
+        return Demo.valueOf(getIntent().getStringExtra(KEY_DEMO));
+    }
+}
